4268f96282cfea2af554747631903a5c3f0a8bd8
Atakan <atakankasbasi@gmail.com>
2023-02-10 17:09:56 +0300

    React Burger Restaurant Project
    Burger Restaurant Web Site Project

---
+++ src/helpers/Data.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import BurgerImage from '../assets/burger.jpg'
+
+export const Data = [
+    {
+        name:'Medium Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 240,
+    },
+    {
+        name:'Medium Cheese Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 245,
+    },
+    {
+        name:'Medium Kasap Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Medium Mexicano Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Steak Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+    {
+        name:'Efso Burger', 
+        image:BurgerImage,
+        content:'150 gr Kasap Burger, Patates, Turşu',
+        price: 250,
+    },
+]
